9dee29d66d96272fe8bb2d1d54214362dfaebaa7
RNHATER <sfrancov@unal.edu.co>
2022-05-13 00:20:52 -0500

    Cuerpito

---
+++ Trabajo 2/ejercicio1.R
@@ -0,0 +1,36 @@
+require(caret)
+
+datos <- read.table("C:\\Users\\SantiagoFranco\\OneDrive - DATAKNOW S.A.S\\Documentos\\Ejercicios-Gestion-de-riesgos\\Trabajo 2\\base6.dat", header = TRUE)
+datos_modelo <- datos[4:11]
+
+##Implementacion modelo logit
+modelo_logit <- glm(Default ~ ., family = binomial, data=datos_modelo)
+###Tabla de coeficientes
+summary(modelo_logit)
+###Valores P
+
+###Pruebas de ajuste
+
+
+###Curva ROC
+
+##Implementacion de random forest
+
+###Curva ROC
+
+##Implementacion de Knn
+
+###Curva ROC
+
+##Implementacion de SVM
+
+###Curva ROC
+
+## Curva ROC para todos los modelos
+## AUC de todos los modelos
+
+##Simulacion con la peor estimacion
+
+##Simulacion con la mejor estimacion
+
+### Comparacion de resultados
